f72d959c186df2dccfa66080be1661f4b37d1de7
Kyle McGuffin <kyle.mcguffin@vumc.org>
2019-05-12 00:04:36 -0400

    Remove extraneous $ from all the $this variables Change db_fetch_row to db_fetch_assoc Ensure random and main project demo variables are matched correctly

--- SurveyRandomization.php
+++ SurveyRandomization.php
@@ -4,15 +4,15 @@ namespace Randomization\SurveyRandomization;
 class SurveyRandomization extends \ExternalModules\AbstractExternalModule {
 	## Need to cache these to limit time from identifying available randomization row
 	## And actually saving the randomization value
-	private $randomProject;
-	private $randomizedField;
+	public $randomProject;
+	public $randomizedField;
 
 	public function __construct() {
 		parent::__construct();
 		// Other code to run when object is instantiated
 	}
 	
-	public function redcap_save_record( int $project_id, string $record = NULL, string $instrument, int $event_id, int $group_id, string $survey_hash = NULL, int $response_id, int $repeat_instance = 1 ) {
+	public function redcap_save_record( int $project_id, string $record = NULL, string $instrument, int $event_id, int $group_id, string $survey_hash = NULL, int $response_id, int $repeat_instance ) {
 		$demoFields = $this->getProjectSetting("this_demo_field");
 		$randomDemoFields = $this->getProjectSetting("that_demo_field");
 
@@ -20,12 +20,12 @@ class SurveyRandomization extends \ExternalModules\AbstractExternalModule {
 		$randomMappedFields = $this->getProjectSetting("that_mapped_field");
 
 		$randomizedField = $this->getProjectSetting("this_record_field");
-		$this->$randomizedField = $this->getProjectSetting("that_record_field");
+		$this->randomizedField = $this->getProjectSetting("that_record_field");
 
-		$this->$randomProject = $this->getProjectSetting("randomization_project");
+		$this->randomProject = $this->getProjectSetting("randomization_project");
 
 		## Check to make sure project is configured before continuing
-		if($this->$randomProject == "" || $randomizedField == "" || $this->$randomizedField == ""
+		if($this->randomProject == "" || $randomizedField == "" || $this->randomizedField == ""
 				|| count($randomMappedFields) == 0) {
 			return;
 		}
@@ -65,8 +65,8 @@ class SurveyRandomization extends \ExternalModules\AbstractExternalModule {
 			## In order to find the next record to be used to randomize
 			$sql = "SELECT record,field_name,event_id,value
 					FROM redcap_data
-					WHERE project_id = ".db_escape($this->$randomProject)."
-						AND field_name IN ('".db_escape($this->$randomizedField)."'";
+					WHERE project_id = ".db_escape($this->randomProject)."
+						AND field_name IN ('".db_escape($this->randomizedField)."'";
 
 			foreach($randomDemoFields as $fieldName) {
 				$sql .= ",'".db_escape($fieldName)."'";
@@ -74,25 +74,23 @@ class SurveyRandomization extends \ExternalModules\AbstractExternalModule {
 
 			$sql .= ")";
 
-			echo $sql;
-
 			$q = $this->query($sql);
 
 			$randomizationData = [];
 			$randomizationEvent = false;
-			while($row = db_fetch_row($q)) {
+			while($row = db_fetch_assoc($q)) {
 				$randomizationEvent = $row['event_id'];
 				$randomizationData[$row['record']][$row['field_name']] = $row['value'];
 			}
 			$chosenRandomizationRecord = false;
 
 			foreach($randomizationData as $randomizationRecord => $recordDetails) {
-				if($recordDetails[$this->$randomizedField] != "") {
+				if($recordDetails[$this->randomizedField] != "") {
 					continue;
 				}
 
-				foreach($demoFields as $fieldName) {
-					if($recordDetails[$fieldName] != $demoValues[$fieldName]) {
+				foreach($demoFields as $demoIndex => $fieldName) {
+					if($recordDetails[$randomDemoFields[$demoIndex]] != $demoValues[$fieldName]) {
 						continue 2;
 					}
 				}
@@ -117,7 +115,7 @@ class SurveyRandomization extends \ExternalModules\AbstractExternalModule {
 				FROM redcap_data
 				WHERE project_id = ".db_escape($this->randomProject)."
 					AND record = '".db_escape($randomizationRecord)."'
-					AND field_name = '".db_escape($this->$randomizedField)."'";
+					AND field_name = '".db_escape($this->randomizedField)."'";
 
 		$q = $this->query($randomizationCheckSql);
 
@@ -126,7 +124,7 @@ class SurveyRandomization extends \ExternalModules\AbstractExternalModule {
 		}
 
 		## Attempt to save the randomization value
-		$this->saveData($this->randomProject,$randomizationRecord,$randomizationEvent,[$this->randomizedField => $thisRecord]);
+		$results = $this->saveData($this->randomProject,$randomizationRecord,$randomizationEvent,[$this->randomizedField => $thisRecord]);
 
 		## Check to make sure that's still the only record attached to this randomization
 		$q = $this->query($randomizationCheckSql);
@@ -136,7 +134,7 @@ class SurveyRandomization extends \ExternalModules\AbstractExternalModule {
 			$sql = "DELETE FROM redcap_data
 					WHERE project_id = ".db_escape($this->randomProject)."
 						AND record = '".db_escape($randomizationRecord)."'
-						AND field_name = '".db_escape($this->$randomizedField)."'
+						AND field_name = '".db_escape($this->randomizedField)."'
 						AND value = '".$thisRecord."'";
 
 			$this->query($sql);
